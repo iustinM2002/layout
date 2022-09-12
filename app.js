4e5a14027c506f139ff694fca2f2898b88c96d7f
iustinM2002 <iustin.morosanu@hartehanks.com>
2022-09-12 16:13:15 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const emailInput = document.getElementById('email_address');
+const validationIcon = document.getElementsByClassName('valid-img')[0];
+const emailContainer = document.getElementsByClassName('email-input-container')[0]
+// functions declarations
+const validateEmail = (e) =>{
+    const inputValue = e.target.value;
+    const inputMatch = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    
+    if(inputValue.length === 0){
+        console.log('empty');
+        emailContainer.classList.remove('valid');
+        emailContainer.classList.remove('invalid');
+        validationIcon.style.display = 'none'
+    }else if(inputMatch.test(inputValue)){
+        emailContainer.classList.add('valid');
+        emailContainer.classList.remove('invalid');
+        validationIcon.src ='./images/valid.png';
+        validationIcon.style.display = 'block';
+    }else{
+        emailContainer.classList.remove('valid');
+        emailContainer.classList.add('invalid');
+        validationIcon.src ='./images/invalid.png';
+        validationIcon.style.display = 'block';
+    }
+};
+
+// events listeners
+emailInput.addEventListener('input',validateEmail);
